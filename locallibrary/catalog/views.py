884429c0760d56e2e896c80b1d55c585d5009c2b
Ben Newland <benjamin.newland@ndsu.edu>
2024-04-02 21:55:21 -0500

    Created the pages to list all books and authors, as well as the detail pages for the books and authors.

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Book, Author, BookInstance, Genre
+from django.views import generic
 
 def index(request):
     """View function for home page of site."""
@@ -14,18 +15,27 @@ def index(request):
     # The 'all()' is implied by default.
     num_authors = Author.objects.count()
 
-    num_sanderson = Book.objects.filter(author__exact = '1').count()
-
-    num_scifi = Book.objects.filter(genre__exact = '1').count()
 
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
-        'num_scifi': num_scifi,
-        'num_sanderson': num_sanderson,
     }
 
     # Render the HTML template index.html with the data in the context variable
     return render(request, 'index.html', context=context)
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 10
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_by = 10
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
